f087fb618d96a0dace56f5d7d827c4b7bc4a4e71
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-09-01 12:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,27 +9,28 @@
 
 // function SportsCar(make, model, topSpeed) {}
 // const user = new Car("Ferrari", "Testarossa")
+
 function Car(make, model) {
 	this.make = make;
 	this.model = model;
 	this.getMakeModel = function () {
-		return console.log(this.make + " " + this.model);
+		return this.make + " " + this.model;
 	}
 }
 
 function SportsCar(make, model, topSpeed) {
     this.topSpeed = topSpeed;
     this.getTopSpeed = function(){
-      console.log(this.topSpeed);
+      return this.topSpeed;
     }
     Car.call(this,make,model)
 }
 // SportsCar.prototype.__proto__ = Car.prototype;
-const user = new Car("Ferrari", "Testarossa")
-user.getMakeModel();
-let user1 = new SportsCar("Tata", "Testarossa", 200)
-user1.getMakeModel();
-user1.getTopSpeed()
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel())
+console.log(car.getTopSpeed())
+
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
